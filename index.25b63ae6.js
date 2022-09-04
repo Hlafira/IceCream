c48b2174b1ac07adee746a3cd2fd2f7a7569afb2
Hlafira <107553024+Hlafira@users.noreply.github.com>
2022-09-04 13:41:25 +0000

    Deploying to gh-pages from @ Hlafira/ice-cream@d802469b10b9b23614acc4c09a9367c3813c019d 🚀

---
+++ index.25b63ae6.js
@@ -0,0 +1,2 @@
+$(".one-time").slick({dots:!0,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!1,arrows:!1});
+//# sourceMappingURL=index.25b63ae6.js.map
---
+++ index.999efb8d.js
@@ -0,0 +1,2 @@
+$(".one-time").slick({dots:!0,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!1,arrows:!1});
+//# sourceMappingURL=index.999efb8d.js.map
