5dc00ef0f4b44518d1a45a154b582e2c6e839710
Hlafira <107553024+Hlafira@users.noreply.github.com>
2022-09-04 13:40:46 +0300

    Revert "correct gallerry slider"
    This reverts commit b44b4cf6b9da7a8744811a5341eba1df0f174c63.

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,19 +1,7 @@
 $('.one-time').slick({
-  dots: true,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  adaptiveHeight: false,
-  arrows: false,
-});
-
-$('.gallery-photo-container').slick({
-  dots: false,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  adaptiveHeight: false,
-  arrows: false,
-  autoplay: true,
-  autoplaySpeed: 2000,
-});
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
