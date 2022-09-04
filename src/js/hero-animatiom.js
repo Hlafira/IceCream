81282aba58307015b4233cbf28d28f84ecae4a5b
Hlafira <107553024+Hlafira@users.noreply.github.com>
2022-09-04 17:10:47 +0300

    change animation

--- src/js/hero-animatiom.js
+++ src/js/hero-animatiom.js
@@ -1,15 +1,12 @@
 const element = document.querySelector('.hero__title');
 const element2 = document.querySelector('.hero__main-img');
 
-element2.style.setProperty('opacity', '0');
-
 element.classList.add('animate__animated', 'animate__zoomInDown');
 element.style.setProperty('--animate-duration', '0.1s');
 
 element.addEventListener('animationend', () => {
-  // do something
-  //   const element2 = document.querySelector('.hero__main-img');
-  element2.style.setProperty('opacity', '1');
-  element2.classList.add('animate__animated', 'animate__zoomIn');
+  // element2.classList.add('visible');
+
+  element2.classList.add('animate__animated', 'animate__fadeInUp'); //'animate__zoomIn');
   element2.style.setProperty('--animate-duration', '0.3s');
 });
