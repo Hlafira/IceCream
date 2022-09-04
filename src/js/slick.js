b44b4cf6b9da7a8744811a5341eba1df0f174c63
Hlafira <107553024+Hlafira@users.noreply.github.com>
2022-09-04 12:41:19 +0300

    correct gallerry slider

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,7 +1,19 @@
 $('.one-time').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true
-  });
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: false,
+  arrows: false,
+});
+
+$('.gallery-photo-container').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: false,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
