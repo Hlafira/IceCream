95b5f7177e1ebb5028c1d1a6fddfec57939b0b7a
Hlafira <107553024+Hlafira@users.noreply.github.com>
2022-09-05 17:32:47 +0300

    add js our locations

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,4 +1,5 @@
 (() => {
+  // Buy now
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     openModalBtnMenu: document.querySelector('[data-modal-open-menu]'),
@@ -13,5 +14,21 @@
   function toggleModal() {
     document.body.classList.toggle('modal-open');
     refs.modal.classList.toggle('is_hidden');
+  }
+
+  // Our location
+  const refsOurLocation = {
+    openModalBtn: document.querySelector('[location-modal__open-btn]'),
+
+    closeModalBtn: document.querySelector('[location-modal__close-button]'),
+    modal: document.querySelector('[location-modal]'),
+  };
+
+  refsOurLocation.openModalBtn.addEventListener('click', toggleModalLocation);
+  refsOurLocation.closeModalBtn.addEventListener('click', toggleModalLocation);
+
+  function toggleModalLocation() {
+    document.body.classList.toggle('modal-open');
+    refsOurLocation.modal.classList.toggle('is_hidden');
   }
 })();
