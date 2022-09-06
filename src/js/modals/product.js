7850b4dbcf950257902f0b2ebaf93fd1d263bdc2
Hlafira <107553024+Hlafira@users.noreply.github.com>
2022-09-06 22:57:32 +0300

    add js products

--- src/js/modals/product.js
+++ src/js/modals/product.js
@@ -2,25 +2,29 @@ import { OpenClose } from './openClose';
 
 (() => {
   // Product
-  if (
-    document.querySelector('[data-modal-open="product-modal"]') &&
-    document.querySelector('#product-modal [data-modal-close]')
-  ) {
-    const refsProduct = {
-      openModalBtn: document.querySelector('[data-modal-open="product-modal"]'),
-      closeModalBtn: document.querySelector(
-        '#product-modal [data-modal-close]'
-      ),
-      modal: document.querySelector('#product-modal'),
-    };
 
-    refsProduct.openModalBtn.addEventListener('click', toggleModalProduct);
-    refsProduct.closeModalBtn.addEventListener('click', toggleModalProduct);
+  const refsProduct = {
+    openModalBtnS: document.querySelectorAll(
+      '[data-modal-open="product-modal"]'
+    ),
+    closeModalBtn: document.querySelector('#product-modal [data-modal-close]'),
+    modal: document.querySelector('#product-modal'),
+  };
+  refsProduct.openModalBtnS.forEach(element => {
+    const name = element.parentElement.querySelector('h4').innerText;
 
-    function toggleModalProduct() {
-      document.body.classList.toggle('modal-open');
-      refsOurLocation.modal.classList.toggle('is_hidden');
-      OpenClose();
-    }
+    element.addEventListener('click', () => {
+      toggleModalProduct(name);
+    });
+  });
+  //refsProduct.openModalBtnS.addEventListener('click', toggleModalProduct);
+  refsProduct.closeModalBtn.addEventListener('click', toggleModalProduct);
+
+  function toggleModalProduct(some) {
+    alert(some);
+    document.body.classList.toggle('modal-open');
+    refsProduct.modal.classList.toggle('is_hidden');
+    refsProduct.modal.querySelector('p').innerHTML = some;
+    OpenClose();
   }
 })();
